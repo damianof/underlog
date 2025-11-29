045f11709f2dffbf75b97725ddef82244c7083c1
damianof <damianof@users.noreply.github.com>
2025-11-29 07:41:12 -0500

    remove dist

--- dist/underlog/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './timeservices';
-export * from './transports';
-export * from './Logger';
-export * from './ILogTransport';
-export * from './ITimestampService';
--- dist/underlog/transports/DefaultTransport.d.ts
+++
@@ -1,9 +0,0 @@
-import { ILogTransportWriteParams } from '../ILogTransport';
-import { BaseTransport } from './BaseTransport';
-export declare class DefaultTransport extends BaseTransport {
-    constructor(params: {
-        level: string;
-        levelOnly: boolean;
-    });
-    write(params: ILogTransportWriteParams): Promise<boolean>;
-}
